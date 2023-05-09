37078c4bce11a6cf380317e6d17c1e3b8a393224
Vincent Yono <vincentyono@protonmail.com>
2023-05-09 11:18:30 +0700

    Implement Search component

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,3 +1,27 @@
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+import styles from '@/styles/components/Search.module.scss';
+
 export default function Search() {
-  return <></>;
+  const [active, setActive] = useState(true);
+  return (
+    <div className={styles.container}>
+      <i className='bi bi-search' />
+      {active ? (
+        <motion.input
+          style={{
+            fontSize: '1.125rem',
+            backgroundColor: 'inherit',
+            padding: '0.5rem 0.5rem',
+            borderRadius: '5px',
+            border: 'none',
+          }}
+          type='text'
+          placeholder='Search...'
+        />
+      ) : (
+        <span>Search</span>
+      )}
+    </div>
+  );
 }
