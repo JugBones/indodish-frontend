615b8f373341f203bc5b937f6ce861eab649cccb
Vincent Yono <vincentyono@protonmail.com>
2023-05-09 11:19:00 +0700

    Add MostPopular section

---
+++ src/sections/MostPopular.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/sections/MostPopular.module.scss';
+
+export default function MostPopular() {
+  return (
+    <section className={styles.container}>
+      <div>
+        <h2>Most Popular</h2>
+      </div>
+    </section>
+  );
+}
