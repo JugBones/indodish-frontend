d0a147d7831e4885c9e31d53e29bacfdc3c95205
JugBones <christopher.tjiandra@binus.ac.id>
2023-05-30 22:30:32 +0700

    add new pages for cuisine
    just adding the page but still empty

--- src/components/PopFoodCard.tsx
+++ src/components/PopFoodCard.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/components/PopFoodCard.module.scss";
 
 const PopFoodCard: React.FC = () => {
   return (
     <div className={styles.container}>
       <Image src="/img/martabak.jpeg" alt="" width={500} height={500} />
-      <h1 className={styles.title}>Matabak</h1>
+      {/* <Link href="/cuisine">Martabak</Link>  */}
+      <h1 className={styles.title}>
+      <Link href="/cuisine">Martabak</Link> 
+      </h1>
       <span className={styles.rating}>4.9</span>
       <p className={styles.desc}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit.
---
+++ src/pages/cuisine.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Cuisine() {
+    return (
+        <>
+         <Head>
+            <title>IndODish - Cuisine</title>
+
+         </Head>
+        </>
+    );
+}
