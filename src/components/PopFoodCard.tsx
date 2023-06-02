bfcf2f4d6102ad093e163b89f321b4bd66f82dbd
Christopher Owen <91533491+Own20@users.noreply.github.com>
2023-06-02 20:07:21 +0700

    update

--- src/components/PopFoodCard.tsx
+++ src/components/PopFoodCard.tsx
@@ -5,14 +5,27 @@ import styles from "../styles/components/PopFoodCard.module.scss";
 const PopFoodCard: React.FC = () => {
   return (
     <div className={styles.container}>
-      <Image src="/ImgItem/martabak.jpeg" alt="" width={500} height={500}/>
-      <h1 className={styles.title}>
-      <Link href="/cuisine">Martabak</Link> 
-      </h1>
-      <span className={styles.rating}>4.9 ⭐</span>
-      <p className={styles.desc}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit.
-      </p>
+      <div className={styles.martabak}>
+        <Image src="/ImgItem/martabak.jpeg" alt="" width={500} height={500}/>
+        <h1 className={styles.title}>
+        <Link href="/cuisine">Martabak</Link> 
+        </h1>
+        <span className={styles.rating}>4.9 ⭐</span>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      <div className={styles.martabak}>
+        <Image src="/ImgItem/martabak.jpeg" alt="" width={500} height={500}/>
+        <h1 className={styles.title}>
+        <Link href="/cuisine">Martabak</Link> 
+        </h1>
+        <span className={styles.rating}>4.9 ⭐</span>
+        <p className={styles.desc}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+      
     </div>
   );
 };
