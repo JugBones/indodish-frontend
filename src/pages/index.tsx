dbb020602ee0893cf99a3b873e7f0b090f4d1b97
JugBones <christopher.tjiandra@binus.ac.id>
2023-06-03 19:56:21 +0700

    update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ export default function Home() {
       <Hero />
       <BrowseLocation />
       {/* <NearbyRestaurant /> */}
-      <PopularCuisine cuisines={{}} />
+      <PopularCuisine />
       <Footer />
     </>
   );
